74d23f4f2858e2023cdfc88b9e76c0aff87963c3
Pedro Rocha <63920197+pedrorochaOSX@users.noreply.github.com>
2025-03-17 23:30:56 -0300

    Update pedrorochaosx-apps-script.sh

--- pedrorochaosx-apps-script.sh
+++ pedrorochaosx-apps-script.sh
@@ -3,4 +3,4 @@ sudo apt install -y flatpak &&
 sudo apt install -y gnome-software-plugin-flatpak &&
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 echo "2/2 Installing apt packages";
-sudo apt install -y gnome-tweaks neovim python3-neovim transmission file-roller vlc htop audacious cheese;
+sudo apt install -y gnome-tweaks neovim python3-neovim transmission file-roller vlc htop audacious cheese
