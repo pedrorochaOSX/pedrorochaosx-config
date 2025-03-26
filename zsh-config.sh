369c057c7634315e1281bb062444b7c99f0d0438
Pedro Rocha <63920197+pedrorochaOSX@users.noreply.github.com>
2025-03-26 13:39:17 -0300

    Update zsh-config.sh

--- zsh-config.sh
+++ zsh-config.sh
@@ -8,12 +8,67 @@ else
     chsh -s $(which zsh);
 fi
 
+sudo cat <<'catEND' > /usr/share/zsh/functions/Prompts/prompt_pedrorochaosx_setup
+# pedrorochaosx prompt theme
+
+prompt_pedrorochaosx_help () {
+  cat <<'EOF'
+This prompt is color-scheme-able.  You can invoke it thus:
+
+  prompt pedrorochaosx [<color1> [<color2> [<color3>]]]
+
+where the colors are for the user@host background, current working
+directory, and current working directory if the prompt is split over
+two lines respectively.  The default colors are blue, cyan and green.
+This theme works best with a dark background.
+
+Recommended fonts for this theme: nexus or vga or similar.  If you
+don't have any of these, then specify the `plain' option to use 7-bit
+replacements for the 8-bit characters.
+EOF
+}
+
+prompt_pedrorochaosx_setup () {
+  setopt localoptions nowarncreateglobal
+  prompt_pedrorochaosx_color1=${1:-'black'}
+  prompt_pedrorochaosx_color2=${2:-'red'}
+  prompt_pedrorochaosx_color3=${3:-'yellow'}
+
+  base_prompt="%B%F{red}%*%f %F{red}%n%B "
+  post_prompt="%b%f%k"
+
+  setopt localoptions extendedglob
+  base_prompt_no_color="${base_prompt//(%K{[^\\\}]#\}|%k)/}"
+  post_prompt_no_color="${post_prompt//(%K{[^\\\}]#\}|%k)/}"
+
+  add-zsh-hook precmd prompt_pedrorochaosx_precmd
+}
+
+prompt_pedrorochaosx_precmd () {
+  setopt localoptions noxtrace nowarncreateglobal
+  local base_prompt_expanded_no_color base_prompt_etc
+  local prompt_length space_left
+
+  base_prompt_expanded_no_color=$(print -P "$base_prompt_no_color")
+  base_prompt_etc=$(print -P "$base_prompt%(4~|...|)%3~")
+  prompt_length=${#base_prompt_etc}
+
+  path_prompt="%B%F{$prompt_pedrorochaosx_color2}%~%F{red}"  # Ensures single-line prompt
+
+  PS1="$base_prompt$path_prompt %F{red}%#%f $post_prompt"
+  PS2="$base_prompt$path_prompt %_> $post_prompt"
+  PS3="$base_prompt$path_prompt ?# $post_prompt"
+}
+
+prompt_pedrorochaosx_setup "$@"
+catEND
+
 cat << 'catEND' > .zshrc
 # Set up the prompt
 
 autoload -Uz promptinit
 promptinit
-prompt adam1
+prompt pedrorochaosx
 
 setopt histignorealldups sharehistory
 
