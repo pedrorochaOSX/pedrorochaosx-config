14b66e2a24e24aad792cdcbc9acbe447a8adb908
Pedro Rocha <63920197+pedrorochaOSX@users.noreply.github.com>
2025-03-28 10:34:11 -0300

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -3,4 +3,4 @@ sudo apt install -y flatpak &&
 sudo apt install -y gnome-software-plugin-flatpak &&
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
 echo "2/2 - Installing packages";
-sudo apt install -y zsh wget gnome-tweaks neovim python3-neovim transmission file-roller vlc htop audacious cheese;
+sudo apt install -y git zsh wget gnome-tweaks neovim python3-neovim transmission file-roller vlc htop audacious cheese;
