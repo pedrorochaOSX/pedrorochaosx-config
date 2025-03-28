16cdb06799364aafb2d9a4c079c38353a1443a69
Pedro Rocha <63920197+pedrorochaOSX@users.noreply.github.com>
2025-03-28 10:39:45 -0300

    Create git_config.sh

---
+++ git_config.sh
@@ -0,0 +1,12 @@
+cd;
+cat <<'catEND' >> .gitconfig
+[alias]
+	ac = !git add --all && git commit -m
+	ch = !git checkout
+	bls = !git branch -a
+	bnew = !git checkout -b
+	bdel = !git branch -d
+	logf = !git log --pretty=format:'%C(cyan)%H %h%d%n  %C(white)%s%n  %C(red)%cn (%ce) - %C(red)%ch (%cr)%C(reset)%n'
+	logs = !git log --pretty=format:'%C(cyan)%h %C(white)%s %C(red)%cn -  %C(red)%ch %C(reset)'
+	st = !git status -s
+ catEND
