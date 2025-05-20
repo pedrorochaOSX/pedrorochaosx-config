a6f26c0c9629eb3abf7eea0c7741922ffcb1416e
Pedro Rocha <63920197+pedrorochaOSX@users.noreply.github.com>
2025-05-20 02:56:00 -0300

    Delete install-packages.sh

--- install-packages.sh
+++
@@ -1,6 +0,0 @@
-echo "1/2 - Installing Flatpak";
-sudo apt install -y flatpak &&
-sudo apt install -y gnome-software-plugin-flatpak &&
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
-echo "2/2 - Installing packages";
-sudo apt install -y git zsh wget gnome-tweaks neovim python3-neovim transmission file-roller vlc htop audacious cheese;
